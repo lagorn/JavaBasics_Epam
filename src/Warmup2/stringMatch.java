93cb8cfde72d7b77ec5f65c357c973a088453a5c
origrim <lagorn@ro.ru>
2019-07-05 20:57:25 +0400

    Add metod123

--- src/Warmup2/stringMatch.java
+++ src/Warmup2/stringMatch.java
@@ -4,6 +4,9 @@ public class stringMatch {
     public int stringMatch(String a, String b) {
         int arrayLength1 = a.length();
         int arrayLength2 = b.length();
+        int firstElement1 = 0;
+        int firstElement2 = 1;
+
         if(a.length() > b.length()){
             if (arrayLength1 % 2 == 0) {
                 for (int i = 0; i <= arrayLength1; i++) {
