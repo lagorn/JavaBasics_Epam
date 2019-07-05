3c89d14ad1fc41b13ca9619e59bcca94bc6b098c
origrim <lagorn@ro.ru>
2019-07-05 20:19:19 +0400

    Add metod

--- src/Warmup2/stringMatch.java
+++ src/Warmup2/stringMatch.java
@@ -2,8 +2,26 @@ package Warmup2;
 
 public class stringMatch {
     public int stringMatch(String a, String b) {
+        int arrayLength1 = a.length();
+        int arrayLength2 = b.length();
+        if(a.length() > b.length()){
+            if (arrayLength1 % 2 == 0) {
+                for (int i = 0; i <= arrayLength1; i++) {
+                    
+                }
+            }
 
 
-        
+        }
+        else if (a.length() < b.length()){
+            if (arrayLength2 % 2 == 0) {
+                return 4;
+            }
+        }
+        else {
+
+        }
+
+        return 4;
     }
 }
