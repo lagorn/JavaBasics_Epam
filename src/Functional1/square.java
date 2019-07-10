798e7827a3ce4eb12032243a8c12ee8f7f8e6ab3
origrim <lagorn@ro.ru>
2019-07-10 09:54:18 +0400

    New class

--- src/Functional1/square.java
+++ src/Functional1/square.java
@@ -1,9 +1,12 @@
 package Functional1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class square {
     public List<Integer> square(List<Integer> nums) {
-
+        ArrayList <Integer> arrayList = new ArrayList<>(4);
+        while ()
+        return arrayList;
     }
 }
