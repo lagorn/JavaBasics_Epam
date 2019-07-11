8e0fa0e1f6d6f8fbe345f4a1d925946f26950bed
origrim <lagorn@ro.ru>
2019-07-11 17:06:21 +0400

    1

--- src/Functional1/square.java
+++ src/Functional1/square.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class square {
     public List<Integer> square(List<Integer> nums) {
-        ArrayList <Integer> arrayList = new ArrayList<>(4);
+        List <Integer> arrayList = new ArrayList<>(4);
         while ()
         return arrayList;
     }
